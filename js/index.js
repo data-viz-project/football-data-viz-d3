dd41e711ef062da2e4b8f48e82ce20c701434f31
Francesco9932 <72614773+Francesco9932@users.noreply.github.com>
2023-09-28 12:36:05 +0200

    import data + creata struttura js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// read the CSV
+var players_data = await d3.csv("../data/2021-2022-player-stats-refined.csv", data => {
+    return data;
+});
+console.log(players_data);
+
+// map to explain features inside the csv 
+var acronyms = await d3.json("../data/acronyms.json", data => {
+    return data;
+});
+console.log(acronyms);
+
+
+function showDashboard() {
+    scatterPlot(players_data)
+    starPlot(players_data, acronyms);
+}
+
+showDashboard();
---
+++ js/scatterPlot.js
@@ -0,0 +1,3 @@
+function scatterPlot(players_data) {
+
+}
